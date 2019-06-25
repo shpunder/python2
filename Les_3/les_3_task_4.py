f1aff8392295dac448fd5f1b18c668d01ef07fad
igor <monah2@yandex.ru>
2019-06-25 10:22:26 +0300

    Выполнил ДЗ к уроку 3

---
+++ Les_3/les_3_task_4.py
@@ -0,0 +1,25 @@
+# Определить, какое число в массиве встречается чаще всего.
+import random
+
+
+SIZE = 10
+MIN_ITEM = 1
+MAX_ITEM = 9
+array = [random.randint(MIN_ITEM, MAX_ITEM) for i in range(SIZE)]
+print(array)
+
+min_1 = MAX_ITEM
+min_2 = MAX_ITEM
+
+for item in array:
+    if item < min_1:
+        if min_2 > min_1:
+            min_2 = min_1
+        min_1 = item
+    elif item < min_2:
+        min_2 = item
+
+
+print(f'Первое минимальное число {min_1} второе {min_2}')
+
+
